8e3f016cd54301a35708d6b7d5375ea731bbc785
Vladyslav Zachepa <112680328+zachepka@users.noreply.github.com>
2024-12-23 02:15:39 -0330

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const ladder = {
+    step: 0,
+    up: function() {
+      this.step++;
+      return this;
+    },
+    down: function() {
+      this.step--;
+      return this;
+    },
+    showStep: function() {
+      console.log(this.step);
+      return this;
+    }
+  };
+
+  ladder.up().up().down().showStep();
